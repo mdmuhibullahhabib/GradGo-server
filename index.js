85b2ab8e734eec58c63545d103c08c4d3250a0c9
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-07-07 21:17:39 +0600

    index

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ async function run() {
     app.delete('/booked/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
-      const result = await bookingsCollection.deleteOne(query)
+      const result = await bookingsCollection.deleteOne(query);
       res.send(result);
     })
 
