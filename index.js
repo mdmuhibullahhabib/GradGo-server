a96063f26ec3e04fbea67f44ed66b494ea1836a1
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-07-07 21:09:47 +0600

    index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const cors = require('cors')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const { ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
