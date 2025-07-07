e469650cde56360bc64ce816b26db9c575341792
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-07-07 21:13:54 +0600

    index

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ async function run() {
 
     app.get('/booked', async (req, res) => {
       const email = req.query.email;
-      const query = { email: email }
+      const query = { email: email };
       const result = await bookingsCollection.find(query).toArray()
       res.send(result)
     })
